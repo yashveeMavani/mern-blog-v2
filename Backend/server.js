f315f0099971f2871e121f43531fa8b9768a4656
yashveeMavani <117334937+yashveeMavani@users.noreply.github.com>
2024-08-12 09:50:26 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,39 +1,39 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const cors = require("cors")
-const path = require("path")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
 
-const IndexRoute = require("./Routers/index")
-const connectDatabase = require("./Helpers/database/connectDatabase")
-const customErrorHandler = require("./Middlewares/Errors/customErrorHandler")
+const IndexRoute = require("./Routers/index");
+const connectDatabase = require("./Helpers/database/connectDatabase");
+const customErrorHandler = require("./Middlewares/Errors/customErrorHandler");
 
-dotenv.config({
-    path:  './Config/config.env'
-})
+dotenv.config();
 
-connectDatabase()
+connectDatabase();
 
-const app = express() ;
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use("/",IndexRoute)
+app.use("/", IndexRoute);
 
-app.use(customErrorHandler)
+app.use(customErrorHandler);
 
-const PORT = process.env.PORT || 5000 ;
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
 
-app.use(express.static(path.join(__dirname , "public") ))
+const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT,()=>{
+app.use(express.static(path.join(__dirname, "public")));
 
-    console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server running on port  ${PORT} : ${process.env.NODE_ENV}`);
+});
 
-})
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error : ${err}`);
 
-process.on("unhandledRejection",(err , promise) =>{
-    console.log(`Logged Error : ${err}`)
-
-    server.close(()=>process.exit(1))
-})
+  server.close(() => process.exit(1));
+});
